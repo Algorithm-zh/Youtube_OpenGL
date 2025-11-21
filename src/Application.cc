1737c9c157912dd886b72def2d45d0f058e7ff2b
Shawn <zxe_wy@163.com>
2025-11-21 18:08:53 +0800

    创建了顶点缓冲区存储顶点数据

--- src/Application.cc
+++ src/Application.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 int main (int argc, char *argv[]) {
+
   GLFWwindow *window;
   if(!glfwInit()){
     return -1;
@@ -14,18 +16,31 @@ int main (int argc, char *argv[]) {
   }
 
   glfwMakeContextCurrent(window);
-
   //必须在创建opengl渲染上下文之后再初始化glew才可以
   if(glewInit() != GLEW_OK){
     std::cout << "Error init glew" << std::endl;
   }
-
   std::cout << glGetString(GL_VERSION) << std::endl;
+
+  float positions[6] = {
+    -0.5, -0.5,
+    0.5, 0.5,
+    0.5, -0.5
+  };
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 
   while(!glfwWindowShouldClose(window)){
 
     //glclear在OpenGL库里
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
 
