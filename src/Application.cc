39ea106695949301ca2ad8ca3abcf73592d12bb1
Shawn <zxe_wy@163.com>
2025-11-25 13:24:18 +0800

    索引缓冲区

--- src/Application.cc
+++ src/Application.cc
@@ -98,21 +98,34 @@ int main (int argc, char *argv[]) {
   }
   std::cout << glGetString(GL_VERSION) << std::endl;
 
-  float positions[6] = {
+  float positions[] = {
     -0.5, -0.5,
-    0, 0.5,
-    0.5, -0.5
+    0.5, -0.5,
+    0.5, 0.5,
+    -0.5, 0.5
+  };
+
+  unsigned int indices[] = {
+    0, 1, 2,
+    2, 3, 0
   };
 
   //将顶点数据存储到gpu
   unsigned int VBO;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
   //启用顶点属性0并告诉opengl如何解析顶点数据
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
+
+
+  //创建索引缓冲区，记录顶点绘制顺序
+  unsigned int ibo;
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
   ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
@@ -128,7 +141,7 @@ int main (int argc, char *argv[]) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     glfwSwapBuffers(window);
 
